878a08797ba92e0e5bc6604397a4cb9eb913649d
EpYpaY <122013216+EpYpaY@users.noreply.github.com>
2023-08-14 10:57:12 +0200

    Add files via upload

--- Notifier.py
+++ Notifier.py
@@ -3,17 +3,17 @@ import schedule
 from winotify import Notification
 
 def send_notification() :
-    toast = Notification(app_id="F1 Results lastet Race",
+    toast = Notification(app_id="F1 Result lastet Race",
     title = "Who win the race ?",
     msg = "A surprise it's possible ?",
     duration = "long",
     icon = r"F:\f1.png",
     )
 
-    toast.add_actions(label="Click Here!", launch="F:\99_CDE\Python/bnotifier.py")
+    toast.add_actions(label="Click Here!", launch="F:\99_CDE\Python/Program.py")
     toast.show()
 
-schedule.every().monday.at("18:00").do(send_notification)
+schedule.every().sunday.at("18:00").do(send_notification)
 
 while True:
     schedule.run_pending()
